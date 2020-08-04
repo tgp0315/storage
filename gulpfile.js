930228f9691b97776208b89d737c67858333627f
v_tianguoping <v_tianguoping@100tal.com>
2020-08-05 00:09:14 +0800

    fix:修改gulpfile文件

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const ts = require("gulp-typescript");
 const tsProject = ts.createProject("tsconfig.json");
 
 gulp.task("default", function() {
-  return tsProject.src()
+  return gulp.src("./index.ts")
     .pipe(tsProject())
     .pipe(gulp.dest("dist"))
 })
